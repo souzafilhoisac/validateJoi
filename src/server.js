d60e0aa4dab15f12aa5b1cb1e0e0a86d457910da
Isac Souza Filho <souzafilhoisac@gmail.com>
2023-01-17 17:55:51 -0300

    rename the router to route

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const server = new express();
+
+const userRoute = require('./routes/userRoutes')
+const errorHandler = require('./middlewares/errorHandler')
+
+server.use(userRoute)
+server.use(express.json());
+server.use(errorHandler);
+
+server.listen(3030, () => {console.log('servidor rodando na porta 3030')})
