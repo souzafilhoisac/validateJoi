e920ae2fb30633436ef81d318901dbf01ec58470
Isac Souza Filho <souzafilhoisac@gmail.com>
2023-01-17 12:35:04 -0300

    implements dbContext to use like a test database

---
+++ src/database/dbContext.js
@@ -0,0 +1,21 @@
+const DbContext = {
+  users: [
+    {
+      id: 1,
+      name: "João",
+      lastName: "da Silva",
+    },
+    {
+      id: 2,
+      name: "Maria",
+      lastName: "Pereira",
+    },
+    {
+      id: 3,
+      name: "Joaquim",
+      lastName: "Araujo",
+    },
+  ],
+};
+
+module.exports = DbContext;
