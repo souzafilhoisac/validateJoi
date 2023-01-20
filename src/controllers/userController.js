d10de214468bef6684ee61c2c2be1aa1134694db
Isac Souza <isasouza@emeal.nttdata.com>
2023-01-20 13:17:54 -0300

    add good pratice, remove desestructuring of argument, using asynchrony and using validation in create method

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,24 +1,36 @@
-const dbContext = require('../database/dbContext');
+const dbContext = require("../database/dbContext");
 const users = dbContext.users;
 
 const userController = {
-  getAll: function () {
-    const [_req, res, next] = arguments;
+  getAll: async (_req, res, next) => {
     try {
-
+      // const users = await dbContextModel.findAll();
       return res.status(200).json(users);
     } catch (err) {
       next(err);
     }
   },
-  create: function () {
-    const [ req, res, next ] = arguments;
+  create: async (req, res, next) => {
     try {
-      const {id, name, lastName, email, password} = req.body;
+      const { id, name, lastName, email, password } = req.body;
 
-      const newUser = { id, name, lastName, email, password };
+      const newUser = /*await dbContextModel.create*/ {
+        id,
+        name,
+        lastName,
+        email,
+        password,
+      };
 
-      users.push(newUser)
+      const userFound = users.find((user) => {
+        return user.id === newUser.id;
+      });
+
+      if (userFound) {
+        return res.status(401).json({ Message: "This User id already exists" });
+      }
+
+      users.push(newUser);
 
       return res.status(200).json(users);
     } catch (err) {
