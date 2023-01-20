92f190fe1fa2c2c02252cc0436697c34fb40f93a
Isac Souza <isasouza@emeal.nttdata.com>
2023-01-20 10:39:01 -0300

    change the message error

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,6 @@
-function errorHandler (err, req, res, next) {
-    console.error(err)
-    res.status(500).json({Error: 'Ops, algo deu errado!'})
+function errorHandler(err, req, res, next) {
+  console.error(err);
+  res.status(500).json({ Error: "Oops, something is wrong..." });
 }
 
 module.exports = errorHandler;
