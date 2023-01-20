8d86eecc049237146475d814de8ed87853d64583
Isac Souza <isasouza@emeal.nttdata.com>
2023-01-20 10:44:35 -0300

    change the identation to best view]

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,16 +1,22 @@
-const express = require('express');
+const express = require("express");
 const route = express.Router();
 
-const {createUserSchema, /*updateUserSchema*/} = require('../database/schemas');
-const validateInformation = require('../middlewares/validateJoi')
+const {
+  createUserSchema /*updateUserSchema*/,
+} = require("../database/schemas");
+const validateInformation = require("../middlewares/validateJoi");
 
-const userController = require('../controllers/userController')
+const userController = require("../controllers/userController");
 
-route.get('/', userController.getAll);
+route.get("/user", userController.getAll);
 // route.get('/', (req, res) => {
 //   res.send('Hello World!')
 // });
 
-route.post('/', validateInformation(createUserSchema), userController.create)
+route.post(
+  "/user",
+  validateInformation(createUserSchema),
+  userController.create
+);
 
 module.exports = route;
