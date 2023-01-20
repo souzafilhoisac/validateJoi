d387ba9a2071fd93e6ac7847f0567f0df19d95f6
Isac Souza <isasouza@emeal.nttdata.com>
2023-01-20 13:20:28 -0300

    change the identation to better the view and add new rotes, get, put and delete

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,22 +1,17 @@
 const express = require("express");
 const route = express.Router();
 
-const {
-  createUserSchema /*updateUserSchema*/,
-} = require("../database/schemas");
+const { createUserSchema, updateUserSchema } = require("../database/schemas");
 const validateInformation = require("../middlewares/validateJoi");
 
 const userController = require("../controllers/userController");
 
-route.get("/user", userController.getAll);
-// route.get('/', (req, res) => {
-//   res.send('Hello World!')
-// });
-
-route.post(
-  "/user",
-  validateInformation(createUserSchema),
-  userController.create
-);
+route
+  .route("/user")
+  .get(userController.getAll)
+  .get(() => {})
+  .post(validateInformation(createUserSchema), userController.create)
+  .put(validateInformation(updateUserSchema), () => {})
+  .delete(() => {});
 
 module.exports = route;
