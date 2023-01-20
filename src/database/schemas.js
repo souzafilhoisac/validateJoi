7dcc8ebbb180222fa7d25ca165e0a5b58f239bf4
Isac Souza <isasouza@emeal.nttdata.com>
2023-01-20 13:18:27 -0300

    change identation

--- src/database/schemas.js
+++ src/database/schemas.js
@@ -1,15 +1,18 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 module.exports = {
   createUserSchema: Joi.object({
     id: Joi.number().required(),
     name: Joi.string().required().min(3).max(30),
     lastName: Joi.string().required().min(3).max(100),
-    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{6,30}$')),
+    email: Joi.string().email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    }),
+    password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{6,30}$")),
   }),
   // updateUserSchema: Joi.object({
   //   name: Joi.string().required(),
   //   lastName: Joi.string().required(),
   // })
-}
+};
