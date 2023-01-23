3a823a05dfcbf742b9e0b572ecaa5eebf05bab9b
Isac Souza <isasouza@emeal.nttdata.com>
2023-01-23 10:10:30 -0300

    add environmental variable with dotenv

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 
 const server = new express();
 
-const userRoute = require('./routes/userRoutes')
-const errorHandler = require('./middlewares/errorHandler')
+const userRoute = require("./routes/userRoutes");
+const errorHandler = require("./middlewares/errorHandler");
 
 server.use(express.json());
-server.use(userRoute)
+server.use(userRoute);
 server.use(errorHandler);
 
-server.listen(3030, () => {console.log('servidor rodando na porta 3030')})
+server.listen(process.env.SERVER_PORT, () => {
+  console.log(`servidor rodando na porta ${process.env.SERVER_PORT}`);
+});
