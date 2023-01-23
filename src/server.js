b2f5138d96c1ef5f22b44cc50fd5c7db81de0204
Isac Souza <isasouza@emeal.nttdata.com>
2023-01-23 10:51:02 -0300

    adding and setting up cors lib

--- src/server.js
+++ src/server.js
@@ -1,11 +1,21 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 
 const server = new express();
 
 const userRoute = require("./routes/userRoutes");
 const errorHandler = require("./middlewares/errorHandler");
 
+server.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    `http://localhost:${process.env.SERVER_PORT}`
+  );
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  server.use(cors());
+  next();
+});
 server.use(express.json());
 server.use(userRoute);
 server.use(errorHandler);
